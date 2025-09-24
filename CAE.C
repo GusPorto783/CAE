bdaff8add3f790360059b170c29a53dd233d8fec
GusPorto783 <augustohporto@gmail.com>
2025-09-24 17:36:12 -0300

    Opção de saída
    Opção de saída do usuário inclusa no sistema

--- CAE.C
+++ CAE.C
@@ -132,6 +132,7 @@ int main ()
 	printf("\t1 - Registrar nomes\n");
 	printf("\t2 - Consultar nomes\n");
 	printf("\t3 - Deletar nomes\n\n"); 
+	printf("\t4 - Sair do Sistema\n\n");
 	printf("opção: ");//Fim do menu
 	
 	scanf("%d", &opcao); //Armazenando escolha do usuário
@@ -151,6 +152,11 @@ int main ()
 		
 		case 3:
 		deletar();
+		break;
+		
+		case 4:
+		printf("Obrigado por Utilizar o CAE!\n");
+		return 0;
 		break;
 		 
 	    default:
